77e7ab41a42fc27f0f448b7466cf558c4fc2bff2
Nils pettersson <nils-pettsson@outlook.com>
2023-09-28 21:12:33 +0200

    testing new folder structure

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+//export * from "../components/src/providers/themeCreatorProvider";
+//export * from "../components/src/types";
+
+export * from "./src/providers/themeCreatorProvider";
+export * from "./src/types";
+export * from "./src/utils/tailwind";
+export * from "./src/utils/theme-utils";
--- index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./components/src/providers/themeCreatorProvider";
-export * from "./types";
-export * from "./utils/tailwind";
-export * from "./utils/theme-utils";
