b3fe51c257720e6cbbefdb337c7537aa62083561
Nils pettersson <nils-pettsson@outlook.com>
2023-09-30 14:01:25 +0200

    added lighten function

--- src/utils/theme-utils.ts
+++ src/utils/theme-utils.ts
@@ -31,35 +31,53 @@ export function generateValues<Count extends keyof CountMap>(
   return values as GenerateValuesReturn<Count>;
 }
 
-export function darken(hex: string, percent: number): string {
-  // Validate the hex string
+function lighten(hex: string, percent: number): string {
   if (!/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex))
     throw new Error("Invalid hex color code");
+  const adjust = 1 + percent / 100;
 
-  // Calculate the adjustment value
-  const adjust = 1 - percent / 100;
-
-  // Convert shortform hex to longform (e.g., "#FFF" to "#FFFFFF")
   if (hex.length === 4) {
     hex = `#${hex[1]}${hex[1]}${hex[2]}${hex[2]}${hex[3]}${hex[3]}`;
   }
 
-  // Extract the Red, Green, and Blue components from the hex string
   let red = parseInt(hex.slice(1, 3), 16);
   let green = parseInt(hex.slice(3, 5), 16);
   let blue = parseInt(hex.slice(5, 7), 16);
 
-  // Adjust each color component
   red = Math.round(red * adjust);
   green = Math.round(green * adjust);
   blue = Math.round(blue * adjust);
 
-  // Ensure that values are within the valid range [0, 255]
   red = Math.max(0, Math.min(255, red));
   green = Math.max(0, Math.min(255, green));
   blue = Math.max(0, Math.min(255, blue));
 
-  // Convert the RGB components back to a hex string and return it
+  return `#${red.toString(16).padStart(2, "0")}${green
+    .toString(16)
+    .padStart(2, "0")}${blue.toString(16).padStart(2, "0")}`;
+}
+
+export function darken(hex: string, percent: number): string {
+  if (!/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex))
+    throw new Error("Invalid hex color code");
+
+  const adjust = 1 - percent / 100;
+  if (hex.length === 4) {
+    hex = `#${hex[1]}${hex[1]}${hex[2]}${hex[2]}${hex[3]}${hex[3]}`;
+  }
+
+  let red = parseInt(hex.slice(1, 3), 16);
+  let green = parseInt(hex.slice(3, 5), 16);
+  let blue = parseInt(hex.slice(5, 7), 16);
+
+  red = Math.round(red * adjust);
+  green = Math.round(green * adjust);
+  blue = Math.round(blue * adjust);
+
+  red = Math.max(0, Math.min(255, red));
+  green = Math.max(0, Math.min(255, green));
+  blue = Math.max(0, Math.min(255, blue));
+
   return `#${red.toString(16).padStart(2, "0")}${green
     .toString(16)
     .padStart(2, "0")}${blue.toString(16).padStart(2, "0")}`;
